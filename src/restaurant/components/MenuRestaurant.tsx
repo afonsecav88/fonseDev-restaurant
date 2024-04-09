b1f98524a4718ab89004ec7dca57070de76d73b6
Adrián Fonseca Vega <afonsecav88@gmail.com>
2024-04-09 11:47:02 -0300

    fixed responisve in menu restaurant component

--- src/restaurant/components/MenuRestaurant.tsx
+++ src/restaurant/components/MenuRestaurant.tsx
@@ -11,61 +11,65 @@ import { Comida, MenuResturant } from '../models/menuRestaurant';
 
 export const MenuRestaurant = () => {
   return (
-    <div className="flex gap-3  sm:w-full md:w-full lg:w-full flex-wrap p-5 justify-center">
-      {menuRestaurant.map((item: MenuResturant) => {
-        return (
-          <Card key={item.dia} className="flex items-center w-80 py-3 ">
-            <CardHeader className="flex gap-3">
-              <Avatar radius="sm" src="./day-week.svg" />
-              <p className="flex align-middle">{item.dia}</p>
-            </CardHeader>
-            <Divider />
-            {item.comidas.map((itemFood: Comida) => {
-              return (
-                <CardBody key={itemFood.id}>
-                  <div className="inline-flex my-2">
-                    <p className="text-small text-default-500 font-semibold mr-2">
-                      Tipo:
-                    </p>
-                    <p className="text-small text-default-500 ">
-                      {itemFood.tipo}
-                    </p>
-                  </div>
-                  <div className="inline-flex py-1">
-                    <p className="text-small text-default-500 font-semibold mr-2">
-                      Plato:
-                    </p>
-                    <p className="text-small text-default-500 ">
-                      {itemFood.plato}
-                    </p>
-                  </div>
+    <div className="flex flex-col">
+      <div className="flex flex-col items-center font-semibold text-blue-400">
+        Nuestro menu semanal
+      </div>
+      <div className="flex gap-3 sm:w-full md:w-full lg:w-full flex-wrap p-5 justify-center">
+        {menuRestaurant.map((item: MenuResturant) => {
+          return (
+            <Card key={item.dia} className="flex items-center w-80 py-3 ">
+              <CardHeader className="flex gap-3">
+                <Avatar radius="sm" src="./day-week.svg" />
+                <p className="flex align-middle">{item.dia}</p>
+              </CardHeader>
+              <Divider />
+              {item.comidas.map((itemFood: Comida) => {
+                return (
+                  <CardBody key={itemFood.id}>
+                    <div className="inline-flex my-2">
+                      <p className="text-small text-default-500 font-semibold mr-2">
+                        Tipo:
+                      </p>
+                      <p className="text-small text-default-500 ">
+                        {itemFood.tipo}
+                      </p>
+                    </div>
+                    <div className="inline-flex py-1">
+                      <p className="text-small text-default-500 font-semibold mr-2">
+                        Plato:
+                      </p>
+                      <p className="text-small text-default-500 ">
+                        {itemFood.plato}
+                      </p>
+                    </div>
 
-                  <div className="inline-flex center py-1">
-                    <p className="text-small text-default-500 font-semibold mr-2">
-                      Descripción:
-                    </p>
-                    <p className="text-small text-default-500 text-pretty">
-                      {itemFood.descripcion}
-                    </p>
-                  </div>
-                  <Divider className="my-4" />
-                  <div className="flex justify-end">
-                    <p className="text-small text-default-500 font-semibold mr-2">
-                      Precio:
-                    </p>
-                    <p className=" text-small text-default-500">
-                      {' '}
-                      {itemFood.precio}
-                    </p>
-                  </div>
-                </CardBody>
-              );
-            })}
-
-            <CardFooter className="flex flex-col gap-2"></CardFooter>
-          </Card>
-        );
-      })}
+                    <div className="inline-flex center py-1">
+                      <p className="text-small text-default-500 font-semibold mr-2">
+                        Descripción:
+                      </p>
+                      <p className="text-small text-default-500 text-pretty">
+                        {itemFood.descripcion}
+                      </p>
+                    </div>
+                    <Divider className="my-4" />
+                    <div className="flex justify-end">
+                      <p className="text-small text-default-500 font-semibold mr-2">
+                        Precio:
+                      </p>
+                      <p className=" text-small text-default-500">
+                        {' '}
+                        {itemFood.precio}
+                      </p>
+                    </div>
+                  </CardBody>
+                );
+              })}
+              <CardFooter className="flex flex-col gap-2"></CardFooter>
+            </Card>
+          );
+        })}
+      </div>
     </div>
   );
 };
