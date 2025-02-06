aeb6caf9c2bf4e76ccd7ccd5e4d5fc135a6dc4b4
Adrian Fonseca Vega <afonsecav88@gmail.com>
2025-02-06 16:17:52 -0300

    update Header component layout and styling for improved responsiveness

--- src/restaurant/layout/components/Header.tsx
+++ src/restaurant/layout/components/Header.tsx
@@ -3,22 +3,23 @@ import { headerRoutesList } from '../../routes/HeaderRoutesList';
 
 export const Header = () => {
   return (
-    <nav className="flex flex-row justify-center flex-wrap  w-full bg-slate-500 b-1 sticky top-0 z-50">
-      <Link to={'/'} className="flex mr-20">
-        <img className="w-64" src="/logo3.svg" alt="logo" />
-      </Link>
-      <div className="flex sm:gap-3  md:gap-5 mb-3 cursor-pointer items-center">
-        {headerRoutesList.map(({ id, name, path }) => (
-          <NavLink
-            key={id}
-            to={path}
-            className={({ isActive }) => (isActive ? 'navigate-Active' : '')}
-          >
-            <button className="bg-white hover:bg-white hover:text-gray-400 border-solid text-xs  border-1 border-gray-400  w-20 h-8 p-2 rounded-md font-semibold">
-              {name}
-            </button>
-          </NavLink>
-        ))}
+    <nav className="flex flex-row justify-center  flex-wrap w-full  bg-slate-500 b-1 sticky top-0 z-50">
+      <div className="flex justify-center align-middle lg:max-w-[69vw] h-40 lg:h-24 flex-wrap lg:flex-nowrap">
+        <Link to={'/'} className="flex mr-20">
+          <img className="w-64 min-w-64" src="/logo3.svg" alt="logo" />
+        </Link>
+        <div className="flex sm:gap-3  md:gap-5 mb-3 cursor-pointer items-center">
+          {headerRoutesList.map(({ id, name, path }) => (
+            <NavLink
+              key={id}
+              to={path}
+              className={({ isActive }) => (isActive ? 'navigate-Active' : '')}>
+              <button className="bg-white hover:bg-white hover:text-gray-400 border-solid text-xs  border-1 border-gray-400 w-28 h-8 p-2 rounded-md font-semibold">
+                {name}
+              </button>
+            </NavLink>
+          ))}
+        </div>
       </div>
     </nav>
   );
